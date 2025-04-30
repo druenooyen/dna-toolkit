238ab62cca0f3645b538b62a571ba14697c2cd55
druenooyen <75636415+druenooyen@users.noreply.github.com>
2025-04-30 15:00:23 -0700

    Added DNA strand helper functions

---
+++ src/dnaUtils.js
@@ -0,0 +1,19 @@
+
+export function generateComplementaryStrand(sequence) {
+    const complementMap = { A: "T", T: "A", C: "G", G: "C" };
+
+    // Generate complementary strand
+    return sequence
+      .split("")
+      .map((nucleotide) => complementMap[nucleotide])
+      .join("");
+  };
+
+  export function generateReverseStrand(sequence) {
+    return sequence.split("").reverse().join("");
+  }
+
+  export function generateReverseComplement(sequence) {
+    const complement = generateComplementaryStrand(sequence);
+    return generateReverseStrand(complement);
+  }
